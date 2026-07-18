9c2b0d68b4b3d47aaf411add4cccbcffb8c8a305
SOWMIYA V M <sowmiya2k21@gmail.com>
2026-07-18 14:20:29 +0530

    Update reverse_string.py

--- strings/reverse_string.py
+++ strings/reverse_string.py
@@ -3,8 +3,8 @@ index = len(txt) - 1
 while index >= 0:
     print(txt[index])
     index = index - 1
-//explanation
-// using while loop we are reversing a string here 
-// now first we are getting an input from the user then since the length of the text is lets say the word nature then its 6 but usually indexing starts from 0 
-// so we reducing the length of original text and next while lets say index is 5 so 5>=0 then print txt[0] which is for the word nature n then txt[1] means a like that it will move in loop
-// for the loop to continue to the next character we use this index = index - 1 so thats how we reverse a string using a loop.
+#explanation
+# using while loop we are reversing a string here 
+# now first we are getting an input from the user then since the length of the text is lets say the word nature then its 6 but usually indexing starts from 0 
+# so we reducing the length of original text and next while lets say index is 5 so 5>=0 then print txt[0] which is for the word nature n then txt[1] means a like that it will move in loop
+# for the loop to continue to the next character we use this index = index - 1 so thats how we reverse a string using a loop.
